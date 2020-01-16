5690a522239b6370460fa2fc970122bbb3b44bae
juan ocampo <>
2020-01-15 22:27:12 -0500

    Initial project

---
+++ PitchPerfect/PitchPerfect/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  PitchPerfect
+//
+//  Created by juan ocampo on 1/15/20.
+//  Copyright © 2020 juan ocampo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func recordAudio(_ sender: Any) {
+        print("record buttom was pressed")
+    }
+    
+}
+
